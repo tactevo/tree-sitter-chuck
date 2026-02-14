f5c2b205e7b323b990ea201faabc013a42a9abf7
tactevo <53024000+tactevo@users.noreply.github.com>
2026-02-14 15:20:51 -0800

    fix: use .product() syntax for SwiftTreeSitter dependency in test target

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .testTarget(
             name: "TreeSitterChuckTests",
             dependencies: [
-                "SwiftTreeSitter",
+                .product(name: "SwiftTreeSitter", package: "swift-tree-sitter"),
                 "TreeSitterChuck",
             ],
             path: "bindings/swift/TreeSitterChuckTests"
